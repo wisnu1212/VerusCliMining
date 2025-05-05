5ab35cc16fd99608bd34af666ee6642f72bfdd3a
RetroMike <michaeldlesk@gmail.com>
2025-05-05 15:51:46 -0500

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -7,9 +7,12 @@ chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
 cd ~
-echo "screen -dmS miner ~/ccminer/ccminer -a verus -o stratum+tcp://luckpool.net:3960 -u RKbgnfWAbL43K1HrxLoWcDzRmTspYFRj6U -p x -t 6" > startup.sh
+echo "screen -dmS miner ~/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKbgnfWAbL43K1HrxLoWcDzRmTspYFRj6U -p x -t 6" > startup.sh
 chmod +x startup.sh
 mkdir -p ~/.termux/boot
 wget https://raw.githubusercontent.com/TheRetroMike/VerusCliMining/refs/heads/main/termux_boot_startup.sh -O ~/.termux/boot/termux_boot_startup.sh
 ~/startup.sh
-screen -r miner
+echo "------------------------------"
+echo "Miner is installed and running. Update config using nano startup.sh"
+echo "To view miner now or after restart, use screen -r miner"
+echo "------------------------------"
