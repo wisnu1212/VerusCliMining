7281a8d0af3614c613026bee93b73eefe4e6ce18
RetroMike <michaeldlesk@gmail.com>
2025-05-05 15:29:10 -0500

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -9,6 +9,7 @@ chmod +x autogen.sh
 cd ~
 echo "screen -dmS miner ~/ccminer/ccminer -a verus -o stratum+tcp://luckpool.net:3960 -u RKbgnfWAbL43K1HrxLoWcDzRmTspYFRj6U -p x -t 6" > startup.sh
 chmod +x startup.sh
+mkdir -p ~/.termux/boot
 echo "#!/data/data/com.termux/files/usr/bin/sh" > ~/.termux/boot/startup.sh
 echo "~/startup.sh" >> ~/.termux/boot/startup.sh
 screen -r miner
